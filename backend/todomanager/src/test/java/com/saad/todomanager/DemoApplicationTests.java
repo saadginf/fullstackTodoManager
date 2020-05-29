b6672137fc877c8e7fdf2109f3b9dfd61c8c2b99
saadginf <saadabbou89@gmail.com>
2020-05-29 15:57:40 +0000

    first commit

---
+++ backend/todomanager/src/test/java/com/saad/todomanager/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saad.todomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
